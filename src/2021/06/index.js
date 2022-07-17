1d2eb2ddc7606e7493912fec8ac683c0d3331435
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2022-07-17 13:05:22 +0100

    feat: day 06

---
+++ src/2021/06/index.js
@@ -0,0 +1,12 @@
+export default function sumPairs(numbers, result) {
+  while (numbers.length > 0) {
+    const number = numbers[0]
+    numbers.shift()
+
+    const pair = numbers.find((value) => value + number === result)
+
+    if (pair) return [number, pair]
+  }
+
+  return null
+}
---
+++ src/2021/06/index.test.js
@@ -0,0 +1,21 @@
+import { deepStrictEqual } from 'node:assert'
+import { describe, it } from 'node:test'
+import sumPairs from './index.js'
+
+describe('2021/01', () => {
+  it('Ejemplo 01', () => {
+    deepStrictEqual(sumPairs([-3, -2, 7, -5], 10), null)
+  })
+
+  it('Ejemplo 02', () => {
+    deepStrictEqual(sumPairs([2, 2, 3, 1], 4), [2, 2])
+  })
+
+  it('Ejemplo 03', () => {
+    deepStrictEqual(sumPairs([6, 7, 1, 2], 8), [6, 2])
+  })
+
+  it('Ejemplo 04', () => {
+    deepStrictEqual(sumPairs([0, 2, 2, 3, -1, 1, 5], 6), [1, 5])
+  })
+})
