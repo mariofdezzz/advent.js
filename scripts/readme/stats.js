4ad7b76515d01894c5b34a04ae4ceaff1f429218
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2022-07-29 14:27:26 +0100

    feat(scripts): get test results

--- scripts/readme/stats.js
+++ scripts/readme/stats.js
@@ -3,6 +3,27 @@ export default class Stats {
     this._tapInput = tapInput.split('\n')
   }
 
+  getTests() {
+    return this._tapInput
+      .filter(
+        (line) =>
+          line.startsWith('ok') ||
+          line.startsWith('  duration_ms')
+      )
+      .reduce((acc, val, idx, arr) => {
+        const day = val.match(/(?<=^ok.+\/)\d{2}(?=\/)/)
+
+        if (day) {
+          acc[+day[0]] = {
+            passed: true,
+            duration: this._getResult(arr[idx + 1]),
+          }
+        }
+
+        return acc
+      }, {})
+  }
+
   getGlobal() {
     const lastItem = this._tapInput.length
     const globals = this._tapInput.slice(
